603c0e5af095b3e4f3903d3f4879e9b143c8e8f0
weiyi <weiyiboynextdoor@163.com>
2026-02-27 21:09:26 +0800

    send chat message

--- main_ui.gd
+++ main_ui.gd
@@ -5,6 +5,7 @@ const CONNECT_TIMEOUT := 5.0
 var ws := WebSocketPeer.new()
 var ws_state := WebSocketPeer.STATE_CLOSED
 var connect_timer := 0.0
+var session_id: String = ""
 
 
 func _process(delta: float) -> void:
@@ -26,6 +27,8 @@ func _process(delta: float) -> void:
 				# create session
 				print("websocket connected, creating session...")
 				ws.send_text(JSON.stringify({"action": "create_session"}))
+			while ws.get_available_packet_count() > 0:
+				process_websocket_message()
 		WebSocketPeer.STATE_CLOSING:
 			$btn_connect.text = "disconnecting..."
 			$btn_connect.disabled = true
@@ -33,11 +36,30 @@ func _process(delta: float) -> void:
 			connect_timer = 0.0
 			$btn_connect.text = "connect"
 			$btn_connect.disabled = false
+			session_id = ""
+
+
+func process_websocket_message() -> void:
+	var packet := ws.get_packet()
+	var text := packet.get_string_from_utf8()
+	var data: Variant = JSON.parse_string(text)
+
+	if session_id.is_empty():
+		# expect session id
+		if data is Dictionary and data.has("session_id"):
+			session_id = data["session_id"]
+			print("session_id: ", session_id)
+	else:
+		if data is Dictionary and data.get("event") == "text_response":
+			$text_assistant_response.text = "Kobe: " + data.get("content")
 
 
 func _on_btn_send_pressed() -> void:
 	# $audio_player.play()
-	pass
+	if ws_state == WebSocketPeer.STATE_OPEN and session_id != "":
+		var input_text : String = $text_user_input.text.strip_edges()
+		var message := {"action": "chat", "session_id": session_id, "message": input_text}
+		ws.send_text(JSON.stringify(message))
 
 
 func _on_btn_connect_pressed() -> void:
