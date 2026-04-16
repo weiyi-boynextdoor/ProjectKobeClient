e4d8f992470bc4d35240e1834d924bc944af3677
weiyi <weiyiboynextdoor@163.com>
2026-04-16 15:43:55 +0800

    text layout

--- user_interface/chat_item_other.gd
+++ user_interface/chat_item_other.gd
@@ -1,6 +1,6 @@
 extends Control
 
-const MAX_TEXT_WIDTH := 800.0
+const MAX_TEXT_WIDTH := 750.0
 const MIN_ITEM_HEIGHT := 80.0
 
 @onready var text_label: Label = $HBoxContainer/text
@@ -15,19 +15,25 @@ func _ready() -> void:
 	_update_text_layout()
 
 
-func _update_text_layout() -> void:
-	var font: Font = text_label.get_theme_font("font")
-	var font_size: int = text_label.get_theme_font_size("font_size")
-	var content_width := 0.0
+func _update_text_layout() -> void:    
+	var font := text_label.get_theme_font("font")
+	var font_size := text_label.get_theme_font_size("font_size")
+	
+	var real_text_size = font.get_multiline_string_size(
+		text_label.text, 
+		text_label.horizontal_alignment, 
+		-1,
+		font_size
+	)
+	
+	var target_width = minf(MAX_TEXT_WIDTH, real_text_size.x)
+	
+	text_label.custom_minimum_size.x = target_width
+	
+	if real_text_size.x > MAX_TEXT_WIDTH:
+		text_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+	else:
+		text_label.autowrap_mode = TextServer.AUTOWRAP_OFF
 
-	for line in text_label.text.split("\n"):
-		content_width = maxf(
-			content_width,
-			font.get_string_size(line, HORIZONTAL_ALIGNMENT_LEFT, -1, font_size).x
-		)
-
-	var target_width := minf(MAX_TEXT_WIDTH, ceilf(content_width) + 1.0)
-	text_label.custom_minimum_size.x = maxf(1.0, target_width)
-	custom_minimum_size.y = maxf(MIN_ITEM_HEIGHT, text_label.get_combined_minimum_size().y)
 	text_label.update_minimum_size()
 	update_minimum_size()
